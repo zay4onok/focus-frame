59f9130f7f7ea8299ce11787dce2eac1683f8d50
Dmitry <dmitronus2015@gmail.com>
2024-12-21 16:01:41 +0200

    add base into vite.cfg

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: '/goit-html-css-project/',
     build: {
       sourcemap: true,
       rollupOptions: {
