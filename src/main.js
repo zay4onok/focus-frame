eca2e906635afbf33ff27ec4b016a8e37ee06700
Dmitry <dmitronus2015@gmail.com>
2024-12-24 21:14:06 +0200

    add reviews 1280

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-$('.reviews__list').slick({
-  dots: true,
-  arrows: false,
-});
+function initSlick() {
+  if ($(window).width() < 1280) {
+    if (!$('.reviews__list').hasClass('slick-initialized')) {
+      $('.reviews__list').slick({
+        dots: true,
+        arrows: false,
+      });
+    }
+  } else {
+    if ($('.reviews__list').hasClass('slick-initialized')) {
+      $('.reviews__list').slick('unslick');
+    }
+  }
+}
+
+$(document).ready(initSlick);
+$(window).on('resize', initSlick);
