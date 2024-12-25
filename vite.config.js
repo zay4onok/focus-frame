bf668657fba982ea1a783c3aaa9dddca9de309e7
Dmitry <dmitronus2015@gmail.com>
2024-12-25 17:24:50 +0200

    fix scripts

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,12 @@ export default defineConfig(({ command }) => {
     build: {
       sourcemap: true,
       rollupOptions: {
+        external: [
+          './src/slick.min.js',
+          './src/jquery.fancybox.min.js',
+          './src/mobile-menu.js',
+          './src/main.js',
+        ],
         input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
